d4c0831e7c332357c2ff7ae75d6709132ff6c936
robin croft <rob@robincroft.com>
2023-08-13 21:21:28 +0100

    add about page

---
+++ src/About.tsx
@@ -0,0 +1,46 @@
+import './App.css'
+
+function About() {
+
+  return (
+
+    <article>
+
+      <section className="description">
+        <h2>Description:</h2>
+        <p>MP Analiser is a comprehensive and user-friendly application designed to analyze the voting records of different members of the UK Parliament...</p>
+      </section>
+
+      <section className="features">
+        <h2>Key Features:</h2>
+        <ul>
+          <li>Voting Analysis</li>
+          <li>Search and Filter</li>
+          <li>Visual Insights</li>
+          <li>Comparative Analysis</li>
+          <li>Historical Context</li>
+          <li>Notifications</li>
+          <li>Data Transparency</li>
+          <li>User-Friendly Interface</li>
+          <li>Custom Reports</li>
+          <li>Secure and Private</li>
+        </ul>
+      </section>
+
+      <section className="how-to-use">
+        <h2>How to Use:</h2>
+        <ol>
+          <li>Search for Members</li>
+          <li>Filter and Analyze</li>
+          <li>Compare and Contrast</li>
+          <li>Set Notifications</li>
+          <li>Generate Reports</li>
+        </ol>
+      </section>
+
+
+    </article>
+  )
+}
+
+export default About
