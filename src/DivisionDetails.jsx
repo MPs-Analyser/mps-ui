23d7b814af20cd4a2ee7b7f32c153617f056010f
robin croft <rob@robincroft.com>
2023-08-20 13:02:56 +0100

    lists of mps for for divisions

--- src/DivisionDetails.jsx
+++ src/DivisionDetails.jsx
@@ -1,11 +1,56 @@
 import * as React from 'react'
+import { FixedSizeList as List } from "react-window";
+import AutoSizer from "react-virtualized-auto-sizer";
 
 import './votingHistory.css';
 
-function DivisionDetails({ onQueryMp, division }) {
+
+const DivisionDetails = ({ onQueryMp, division }) => {
+
+  const [votedAye, setVotedAye] = React.useState(() => [[]]);
+  const [votedNo, setVotedNo] = React.useState(() => [[]]);
+  const [absent, setAbsent] = React.useState(() => [[]]);
+
+  const AyeRow = ({ index, style }) => (
+    <div className={index % 2 ? "ListItemOdd" : "ListItemEven"} style={style}>
+      <span>{votedAye[index].id}</span>      
+      <span>{votedAye[index].name}</span>
+    </div>
+  );
+
+  const NoRow = ({ index, style }) => (
+    <div className={index % 2 ? "ListItemOdd" : "ListItemEven"} style={style}>
+      <span>{votedNo[index].id}</span>      
+      <span>{votedNo[index].name}</span>
+    </div>
+  );
+
+  const AbsentRow = ({ index, style }) => (
+    <div className={index % 2 ? "ListItemOdd" : "ListItemEven"} style={style}>
+      <span>{absent[index].id}</span>      
+      <span>{absent[index].name}</span>
+    </div>
+  );
+
 
   React.useEffect(() => {
-  }, []);
+    console.log('division ', division);
+
+    const ayesList = [];
+    division.Ayes.forEach(i => ayesList.push({ id: i.MemberId, name: i.Name }));
+    setVotedAye(ayesList);
+
+    const noList = [];
+    division.Noes.forEach(i => noList.push({ id: i.MemberId, name: i.Name }));
+    setVotedNo(noList);
+
+    const absentList = [];
+    division.NoVoteRecorded.forEach(i => absentList.push({ id: i.MemberId, name: i.Name }));
+    setAbsent(absentList);
+
+
+    // setData(ayesList)
+  }, [division]);
 
   return (
     <div className="divisionDetails">
@@ -20,52 +65,67 @@ function DivisionDetails({ onQueryMp, division }) {
             <li>{division.Date}</li>
             <li>AyeCount: {division.AyeCount}</li>
             <li>NoCount: {division.NoCount}</li>
+            <li>Absent: {division.NoVoteRecorded.length}</li>
           </ul>
-
-          <section>
-            <h4>Members who voted Aye</h4>
-            <ul>
-              <li>
-                <button
-                  className='button-link'
-                  onClick={() => onQueryMp(division.Ayes[0]?.MemberId)}>
-                  {division.Ayes[0]?.Name}
-                </button>
-              </li>
-              <li>
-                <button
-                  className='button-link'
-                  onClick={() => onQueryMp(division.Ayes[1]?.MemberId)}>
-                  {division.Ayes[1]?.Name}
-                </button>
-              </li>
-              <li>.....</li>
-            </ul>
-
-            <h4>Members who voted No</h4>
-            <ul>
-              <li>
-                <button
-                  className='button-link'
-                  onClick={() => onQueryMp(division.Noes[0]?.MemberId)}>
-                  {division.Noes[0]?.Name}
-                </button>
-              </li>
-              <li>
-                <button
-                  className='button-link'
-                  onClick={() => onQueryMp(division.Noes[1]?.MemberId)}>
-                  {division.Noes[1]?.Name}
-                </button>
-              </li>
-              <li>.....</li>
-            </ul>
-          </section>
-
         </div>
       )}
 
 
+
+      <div className='ayesListWrapper' style={{ height: '300px', overflowX: 'hidden', display: 'flex' }}>
+
+        <div style={{ height: '100%', border: '1px solid', flex: 1 }}>
+          <h3>Voted Aye</h3>
+          <AutoSizer>
+            {({ height, width }) => (
+
+              <List
+                className="List"
+                height={height}
+                itemSize={35}
+                itemCount={votedAye.length}
+                width={width}
+              >
+                {AyeRow}
+              </List>
+            )}
+          </AutoSizer>
+        </div>
+
+        <div style={{ height: '100%', border: '1px solid', flex: 1 }}>
+          <h3>Voted No</h3>
+          <AutoSizer>
+            {({ height, width }) => (
+              <List
+                className="List"
+                height={height}
+                itemCount={votedNo.length}
+                itemSize={35}
+                width={width}
+              >
+                {NoRow}
+              </List>
+            )}
+          </AutoSizer>
+        </div>
+
+        <div style={{ height: '100%', border: '1px solid', flex: 1 }}>
+          <h3>Absent</h3>
+          <AutoSizer>
+            {({ height, width }) => (
+              <List
+                className="List"
+                height={height}
+                itemCount={absent.length}
+                itemSize={35}
+                width={width}
+              >
+                {AbsentRow}
+              </List>
+            )}
+          </AutoSizer>
+        </div>
+      </div>
     </div>
   )
 }
