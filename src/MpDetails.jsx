89f425000383ce82e872c64e6eae2493883ab688
barooahn <barooahn@gmail.com>
2023-09-06 21:25:36 +0100

    added splash and general styling

--- src/MpDetails.jsx
+++ src/MpDetails.jsx
@@ -1,219 +1,407 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
-import VotingHistory from './VotingHistory';
+import VotingHistory from "./VotingHistory";
 
-import PartyLogo from './PartyLogo';
+import PartyLogo from "./PartyLogo";
 
 import "./styles/mpDetails.css";
 
-import ky from 'ky-universal';
-import BarChart from './BarChart';
+import ky from "ky-universal";
+import BarChart from "./BarChart";
 
 import commonsImage from "./assets/commons.png";
 import lordsImage from "./assets/lords.png";
 
-import { config } from '../src/app.config';
+import { config } from "./app.config";
 
-const MpDetails = ({ votingSummary, details, onQueryMpByName, onQueryMp, onQueryDivision, setGlobalMessage }) => {
+const MpDetails = ({
+	votingSummary,
+	details,
+	onQueryMpByName,
+	onQueryMp,
+	onQueryDivision,
+	setGlobalMessage,
+}) => {
+	const [votingSimilarity, setVotingSimilarity] = useState();
+	const [votingHistory, setVotingHistory] = useState();
+	const [barChartData, setBarChartData] = useState();
 
-  const [votingSimilarity, setVotingSimilarity] = useState();
-  const [votingHistory, setVotingHistory] = useState();
-  const [barChartData, setBarChartData] = useState();
+	const [progress, setProgress] = useState();
 
-  const [progress, setProgress] = useState();
+	useEffect(() => {
+		console.log("details ", details);
+		console.log(
+			"chips  ",
+			window
+				.getComputedStyle(document.documentElement)
+				.getPropertyValue("--clr-accent")
+		);
+	}, []);
 
-  useEffect(() => {
-    console.log('details ', details);
-    console.log('chips  ', window.getComputedStyle(document.documentElement).getPropertyValue('--clr-accent'));
-  }, []);
+	const Party = {
+		CONSERVATIVE: "Conservative",
+		LABOUR: "Labour",
+		LIBERAL_DEMOCRATS: "Liberal Democrats",
+		GREEN: "Green Party",
+		SNP: "Scottish National Party",
+		PLAID_CYMRU: "Plaid Cymru",
+		DUP: "Democratic Unionist Party",
+		SINN_FEIN: "Sinn Féin",
+		UUP: "Ulster Unionist Party",
+		SDLP: "Social Democratic and Labour Party",
+		BREXIT_PARTY: "Brexit Party",
+		RECLAIM: "The Reclaim Party",
+		UNKNOWN: "Unknown",
+	};
 
-  const onGetVotingSimilarity = async () => {
+	const getColour = (partyName) => {
+		if (!Object.values(Party).includes(partyName)) {
+			partyName = Party.UNKNOWN;
+		}
 
-    setProgress('Getting voting similarity...')
-    //clear voting history to make space for similarity
-    setVotingHistory(undefined);
+		switch (partyName) {
+			case Party.CONSERVATIVE:
+				return "#0a3b7c";
+			case Party.LABOUR:
+				return "#e4003b";
+			case Party.LIBERAL_DEMOCRATS:
+				return "#fabb00";
+			case Party.GREEN:
+				return "#6ab023";
+			case Party.SNP:
+				return "#ffdf00";
+			case Party.PLAID_CYMRU:
+				return "#008142";
+			case Party.DUP:
+				return "#d46a4c";
+			case Party.SINN_FEIN:
+				return "#326760";
+			case Party.UUP:
+				return "#48a5ee";
+			case Party.SDLP:
+				return "#2aa82c";
+			case Party.BREXIT_PARTY:
+				return "#5b7eb5";
+			case Party.RECLAIM:
+				return "#14172d";
+			case Party.UNKNOWN:
+			default:
+				throw new Error("Unknown party");
+		}
+	};
 
-    setTimeout(() => document.getElementsByClassName('container')[0].scrollTo(0, document.body.scrollHeight), 100);
+	const partyColor = getColour(details.value.latestParty.name);
+	console.log("partyColor", partyColor); // Should print the color of 'The Reclaim Party'
 
-    const result = await ky(`${config.mpsApiUrl}votingSimilarity?name=${details?.value?.nameDisplayAs}`).json();
+	const onGetVotingSimilarity = async () => {
+		setProgress("Getting voting similarity...");
+		//clear voting history to make space for similarity
+		setVotingHistory(undefined);
 
-    setVotingSimilarity(result.similarity);
+		setTimeout(
+			() =>
+				document
+					.getElementsByClassName("container")[0]
+					.scrollTo(0, document.body.scrollHeight),
+			100
+		);
 
-    //TODO something not working getting the css variable for bar colour so using localstorage direct. fix this
-    const chartData = {
-      labels: [],
-      datasets: [
-        {
-          label: 'Voting Similarity',
-          backgroundColor: window.localStorage.getItem("theme") === 'light-mode' ? '#a972cb' : '#980c4c',
-          borderColor: 'rgba(0,0,0,1)',
-          borderWidth: 2,
-          indexAxis: 'y',
-          data: [],
-        }
-      ]
-    }
+		const result = await ky(
+			`${config.mpsApiUrl}votingSimilarity?name=${details?.value?.nameDisplayAs}`
+		).json();
 
-    result.similarity.forEach(element => {
-      console.log(element);
-      chartData.labels.push(element.name)
-      chartData.datasets[0].data.push(element.score)
-    });
+		setVotingSimilarity(result.similarity);
 
+		//TODO something not working getting the css variable for bar colour so using localstorage direct. fix this
+		const chartData = {
+			labels: [],
+			datasets: [
+				{
+					label: "Voting Similarity",
+					backgroundColor:
+						window.localStorage.getItem("theme") ===
+						"light-mode"
+							? "#a972cb"
+							: "#980c4c",
+					borderColor: "rgba(0,0,0,1)",
+					borderWidth: 2,
+					indexAxis: "y",
+					data: [],
+				},
+			],
+		};
 
-    setBarChartData(chartData);
-    setProgress(undefined);
+		result.similarity.forEach((element) => {
+			console.log(element);
+			chartData.labels.push(element.name);
+			chartData.datasets[0].data.push(element.score);
+		});
 
-  }
+		setBarChartData(chartData);
+		setProgress(undefined);
+	};
 
-  const onGetVotingHistory = async (type) => {
+	const onGetVotingHistory = async (type) => {
+		setProgress("Analysing voting history...");
 
-    setProgress('Analysing voting history...')
+		//clear similarity to make space for voting history
+		setVotingSimilarity(undefined);
+		setBarChartData(undefined);
+		setVotingHistory(undefined);
 
-    //clear similarity to make space for voting history
-    setVotingSimilarity(undefined);
-    setBarChartData(undefined);
-    setVotingHistory(undefined);
+		//TODO scroll to bottom probably should be for mobile only
+		setTimeout(
+			() =>
+				document
+					.getElementsByClassName("container")[0]
+					.scrollTo(0, document.body.scrollHeight),
+			1
+		);
 
-    //TODO scroll to bottom probably should be for mobile only
-    setTimeout(() => document.getElementsByClassName('container')[0].scrollTo(0, document.body.scrollHeight), 1);
+		try {
+			const response = await ky(
+				`${config.mpsApiUrl}votingDetails?id=${details?.value?.id}&type=${type}`
+			).json();
+			const formattedResults = [];
 
-    try {
-      const response = await ky(`${config.mpsApiUrl}votingDetails?id=${details?.value?.id}&type=${type}`).json();
-      const formattedResults = [];
+			response.forEach((i) => {
+				console.log("field ", i._fields);
+				// const memberVotedAye = type === "votedAye" ? true : type === "votedNo" ? false : 'dont know';
 
-      response.forEach(i => {
-        console.log('field ', i._fields);
-        // const memberVotedAye = type === "votedAye" ? true : type === "votedNo" ? false : 'dont know';
+				formattedResults.push({
+					divisionId: i.DivisionId,
+					title: i.Title,
+					date: i.Date,
+					memberVotedAye: i.memberVotedAye,
+				});
+			});
 
-        formattedResults.push({
-          divisionId: i.DivisionId,
-          title: i.Title,
-          date: i.Date,
-          memberVotedAye: i.memberVotedAye
-        })
-      });
+			setVotingHistory(formattedResults);
+			setProgress(undefined);
+		} catch (error) {
+			setProgress(undefined);
+			console.error(error);
+			setVotingHistory(undefined);
+			setGlobalMessage({ type: "error", text: error.message });
+		}
+	};
 
-      setVotingHistory(formattedResults);
-      setProgress(undefined);
-    } catch (error) {
-      setProgress(undefined);
-      console.error(error);
-      setVotingHistory(undefined);
-      setGlobalMessage({ type: 'error', text: error.message });
-    }
-  }
+	return (
+		<>
+			<section className='mpDetails'>
+				<div className='mpDetails_image_title'>
+					<img
+						className='mpDetails__image'
+						style={{
+							border: `3px solid ${partyColor}`,
+						}}
+						src={`${details.value?.thumbnailUrl}`}
+					/>
 
-  return (
+					<div className='mpDetails__titleWrapper'>
+						<div className='mpDetails_logo_title'>
+							<PartyLogo
+								backgroundColour={`${partyColor}`}
+								foregroundColour={`#${details?.value?.latestParty?.foregroundColour}`}
+								partyName={
+									details?.value?.latestParty?.name
+								}
+							/>
+							<span>
+								<h4>{details.value.nameDisplayAs}</h4>
+							</span>
+						</div>
 
-    <>
-      <section className="mpDetails">
+						<div className='house__tooltop'>
+							{details.value.latestHouseMembership
+								?.house === 1
+								? "House of Commons"
+								: "House of Lords"}
+						</div>
+						<div className='house__wrapper'>
+							<img
+								className='mpDetails__house'
+								src={
+									details.value.latestHouseMembership
+										?.house === 1
+										? `${commonsImage}`
+										: `${lordsImage}`
+								}
+							/>
+						</div>
+					</div>
+				</div>
 
-        <div className="mpDetails_image_title">
+				<div className='mpDetails__overview'>
+					<table>
+						<tbody>
+							<tr>
+								<th>Constituency</th>
+								<td>
+									{
+										details.value
+											.latestHouseMembership
+											?.membershipFrom
+									}
+								</td>
+							</tr>
+							<tr>
+								<th>House</th>
+								<td>
+									{details.value
+										.latestHouseMembership
+										?.house === 1
+										? "Commons"
+										: "Lords"}
+								</td>
+							</tr>
+							<tr>
+								<th>Member Since</th>
+								<td>
+									{details.value
+										.latestHouseMembership &&
+										details.value.latestHouseMembership?.membershipStartDate.substring(
+											0,
+											10
+										)}
+								</td>
+							</tr>
+							<tr>
+								<th>Status</th>
+								<td>
+									{details.value
+										.latestHouseMembership
+										?.membershipStatus
+										? `Active`
+										: `Inactive`}
+								</td>
+							</tr>
+							<tr>
+								<th>Status Description</th>
+								<td>
+									{
+										details.value
+											.latestHouseMembership
+											?.membershipStatus
+											?.statusDescription
+									}
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
 
-          <img className='mpDetails__image' src={`${details.value?.thumbnailUrl}`} />
+				<div className='mpDetails__actions'>
+					<button
+						className='button'
+						style={{
+							borderColor: `${partyColor}`,
+						}}
+						onClick={onGetVotingSimilarity}
+					>
+						Most Similar Voting Mps
+					</button>
+					{/* <button className="button">Least Similar Voting Mps</button> */}
+					<button
+						className='button'
+						style={{
+							borderColor: `${partyColor}`,
+						}}
+						onClick={() => onGetVotingHistory("all")}
+					>
+						Voting History
+					</button>
 
-          <div className="mpDetails__titleWrapper">
-            <div className="mpDetails_logo_title">
-              <PartyLogo
-                backgroundColour={`#${details?.value?.latestParty?.backgroundColour}`}
-                foregroundColour={`#${details?.value?.latestParty?.foregroundColour}`}
-                partyName={details?.value?.latestParty?.name}
-              />
-              <span>
-                <h4>{details.value.nameDisplayAs}</h4>
-              </span>
+					{votingSummary && (
+						<div className='votingSummary'>
+							<h4>
+								How{" "}
+								{
+									details.value.nameDisplayAs.split(
+										" "
+									)[0]
+								}{" "}
+								voted
+							</h4>
+							<div className='votingSummary__buttons'>
+								<button
+									className='button votingButton'
+									style={{
+										borderColor: `${partyColor}`,
+									}}
+									onClick={() =>
+										onGetVotingHistory("all")
+									}
+								>
+									Total
+								</button>
+								<button
+									className='button'
+									style={{
+										borderColor: `${partyColor}`,
+									}}
+									onClick={() =>
+										onGetVotingHistory("votedAye")
+									}
+								>
+									Aye
+								</button>
+								<button
+									style={{
+										borderColor: `${partyColor}`,
+									}}
+									className='button'
+									onClick={() =>
+										onGetVotingHistory("votedNo")
+									}
+								>
+									No
+								</button>
 
-            </div>
+								<span className='votingSummary__buttons__count'>
+									{votingSummary?.votedAye?.length +
+										votingSummary?.votedNo
+											?.length}
+								</span>
+								<span className='votingSummary__buttons__count'>
+									{votingSummary?.votedAye?.length ||
+										0}
+								</span>
+								<span className='votingSummary__buttons__count'>
+									{votingSummary?.votedNo?.length ||
+										0}
+								</span>
+							</div>
+						</div>
+					)}
+				</div>
+			</section>
 
-            <div className="house__tooltop">
-              {details.value.latestHouseMembership?.house === 1 ? 'House of Commons' : 'House of Lords'}
-            </div>
-            <div className="house__wrapper">
-              <img className='mpDetails__house' src={details.value.latestHouseMembership?.house === 1 ? `${commonsImage}` : `${lordsImage}`} />
-            </div>
-          </div>
-        </div>
+			{votingSimilarity && (
+				<BarChart
+					barChartData={barChartData}
+					onQueryMpByName={onQueryMpByName}
+				/>
+			)}
 
+			{progress && (
+				<>
+					<div className='votingHistoryProgress'>
+						<progress value={null} />
+						<p>{progress}</p>
+					</div>
+				</>
+			)}
 
-        <div className="mpDetails__overview">
-          <table>
-            <tbody>
-              <tr>
-                <th>Constituency</th>
-                <td>{details.value.latestHouseMembership?.membershipFrom}</td>
-              </tr>
-              <tr>
-                <th>House</th>
-                <td>{details.value.latestHouseMembership?.house === 1 ? 'Commons' : 'Lords'}</td>
-              </tr>
-              <tr>
-                <th>Member Since</th>
-                <td>{details.value.latestHouseMembership && details.value.latestHouseMembership?.membershipStartDate.substring(0, 10)}</td>
-              </tr>
-              <tr>
-                <th>Status</th>
-                <td>{details.value.latestHouseMembership?.membershipStatus ? `Active` : `Inactive`}</td>
-              </tr>
-              <tr>
-                <th>Status Description</th>
-                <td>{details.value.latestHouseMembership?.membershipStatus?.statusDescription}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-
-        <div className="mpDetails__actions">
-          <button className="button" onClick={onGetVotingSimilarity}>Most Similar Voting Mps</button>
-          {/* <button className="button">Least Similar Voting Mps</button> */}
-          <button className="button" onClick={() => onGetVotingHistory('all')}>Voting History</button>
-
-          {votingSummary && (
-            <div className="votingSummary">
-              <h4>How {details.value.nameDisplayAs.split(' ')[0]} voted</h4>
-              <div className="votingSummary__buttons">
-
-                <button className="button votingButton" onClick={() => onGetVotingHistory('all')}>Total</button>
-                <button className="button" onClick={() => onGetVotingHistory('votedAye')}>Aye</button>
-                <button className="button" onClick={() => onGetVotingHistory('votedNo')}>No</button>
-
-                <span className='votingSummary__buttons__count'>{votingSummary?.votedAye?.length + votingSummary?.votedNo?.length}</span>
-                <span className='votingSummary__buttons__count'>{votingSummary?.votedAye?.length || 0}</span>
-                <span className='votingSummary__buttons__count'>{votingSummary?.votedNo?.length || 0}</span>
-
-              </div>
-
-            </div>
-          )}
-        </div>
-
-
-
-
-
-      </section>
-
-
-      {votingSimilarity && (
-        <BarChart barChartData={barChartData} onQueryMpByName={onQueryMpByName} />
-      )}
-
-      {progress && (
-        <>
-          <div className="votingHistoryProgress">
-            <progress value={null} />
-            <p>{progress}</p>
-          </div>
-        </>
-      )}
-
-      {votingHistory && (
-        <VotingHistory votingHistory={votingHistory} onQueryMp={onQueryMp} onQueryDivision={onQueryDivision} />
-      )}
-
-    </>
-
-  )
-}
+			{votingHistory && (
+				<VotingHistory
+					votingHistory={votingHistory}
+					onQueryMp={onQueryMp}
+					onQueryDivision={onQueryDivision}
+				/>
+			)}
+		</>
+	);
+};
 
 export default MpDetails;
---
+++ src/Splash.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./styles/splash.css";
+
+function Splash() {
+	return (
+		<>
+			<section className='intro'>
+				<h1>Welcome to UK MPs and Lords Statistics</h1>
+				<p>
+					Discover in-depth statistics, insights, and data-driven
+					analysis about Members of Parliament (MPs) and Lords in
+					the United Kingdom. Our platform provides a
+					comprehensive resource for anyone interested in
+					exploring the political landscape, decision-making
+					processes, and demographics of these esteemed
+					representatives.
+				</p>
+				<p>
+					From the historical evolution of parliamentary roles to
+					current debates and voting trends, we're your go-to
+					source for understanding the intricate workings of the
+					UK's legislative bodies.
+				</p>
+				<p>
+					Explore key metrics such as attendance records, voting
+					history, party affiliations, geographical
+					representation, and more. Our user-friendly search
+					functionality allows you to effortlessly access
+					information about specific MPs and Lords, ensuring you
+					have the knowledge you need at your fingertips.
+				</p>
+				<p>
+					Whether you're a researcher, journalist, student, or
+					simply curious about UK politics, our platform aims to
+					empower you with accurate and up-to-date data,
+					fostering a deeper understanding of the individuals
+					shaping the nation's laws and policies.
+				</p>
+			</section>
+		</>
+	);
+}
+
+export default Splash;
