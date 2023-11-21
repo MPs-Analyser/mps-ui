8aac4bbf8a2c5ec8d0b6db79d75ace2c4c8287aa
robin croft <rob@robincroft.com>
2023-11-21 11:32:44 +0000

    whatever

--- src/app.config.js
+++ src/app.config.js
@@ -1,4 +1,5 @@
 export const config = {
-    // mpsApiUrl: 'https://mps-api-production-1f49.up.railway.app/'                              
-    mpsApiUrl: 'http://localhost:8080/'
+    gitmpsApiUrl: 'https://mps-api-production-1f49.up.railway.app/'                              
+    // mpsApiUrl: 'http://localhost:8080/'
+    //tdo 
 }
