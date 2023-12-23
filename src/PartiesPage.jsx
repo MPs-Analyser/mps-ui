abe9348b477658402547c128908d22ae7ec4e449
robin croft <rob@robincroft.com>
2023-12-23 12:19:19 +0000

    add donations

--- src/PartiesPage.jsx
+++ src/PartiesPage.jsx
@@ -7,26 +7,56 @@ import ky from 'ky-universal';
 
 const Parties = () => {
 
-  const [parties, setParties] = useState();
+  const [parties, setParties] = useState([]);
+  const [donations, setDonations] = useState();
 
   useEffect(() => {
 
-	const getParties = async () => {
-		const response = await ky(`${config.mpsApiUrl}getparties`).json();
-    console.log("got ", parties);
-    
-    setParties(response);
-	}
+    const getParties = async () => {
 
-	getParties();
-    
+      const mpsResponse = await ky(`${config.mpsApiUrl}getparties`).json();
+      setParties(mpsResponse);
+
+      const donationsResponse = await ky(`${config.mpsApiUrl}donations`).json();
+      setDonations(donationsResponse);
+    }
+
+    getParties();
+
   }, []);
 
   return (
     <div>
-      {parties && parties.map(i => (
+
+      <table>
+        <thead>
+          <tr>
+            <th>Party</th>
+            <th>Members Count</th>
+            <th>Donation Count</th>
+            <th>Donation Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {parties && donations && donations.map(i => (
+              <tr>
+                <td>{i.partyName}</td>
+                <td>{parties.find(party => party.name === i.partyName) ? parties.find(party => party.name === i.partyName).mpsCount : ''}</td>
+                <td>{i.donationCount}</td>
+                <td>{i.totalDonationValue}</td>
+              </tr>      
+      ))}
+        </tbody>
+      </table>
+
+
+      {/* {parties && parties.map(i => (
         <p>{i.name}: {i.mpsCount}</p>
       ))}
+
+      {donations && donations.map(i => (
+        <p>{i.partyName}: {i.donationCount} {i.totalDonationValue}</p>
+      ))} */}
     </div>
   )
 }
